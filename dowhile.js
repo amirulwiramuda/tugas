4147ffe7b6e21884e671aefcca2399d5b916beae
Amirul Wiramuda <amirulwiramuda@gmail.com>
2018-11-25 23:44:43 +0700

    soal do while

--- dowhile.js
+++ dowhile.js
@@ -1,4 +1,29 @@
-var jumlah = 20;
-while(jumlah <=20)
-    console.log( 'I love coding');
-    jumlah = jumlah++;
+// var text = "";
+// var i = 0;
+// while (i < 10) {
+//     text += "The number is " + i;
+//     i++;
+// }
+
+// console.log(text)
+
+var loop = ''
+var loop2 = ''
+var i = 2
+var j = 20
+var counter = 2
+
+
+while (i <= 20) {
+  loop += i + ' I love coding' + '\n'
+  i += counter
+}
+
+while (j > 0) {
+  loop2 += j + ' I love coding' + '\n'
+  j -= counter
+}
+console.log('Looping pertama')
+console.log(loop)
+console.log('Looping kedua')
+console.log(loop2)
